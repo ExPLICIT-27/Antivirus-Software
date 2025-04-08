9e8fb89c883321f1fce1dc831149469662b89b72
ExPLICIT-27 <nandumahesh2004@gmail.com>
2025-04-08 20:33:19 +0530

    fixed ttk

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/explicit/OperatingSystemsProjectAVSOFT/venv
+set -gx VIRTUAL_ENV /home/explicit/AntivirusSoftwareYARA/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
